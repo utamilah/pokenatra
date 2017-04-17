cf6c0284481fe8b511aeff30db61ee6ea3378ba9
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-17 16:42:18 -0400

    create file and add routes

---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+# Load the file to connect to the DB
+require_relative 'db/connection'
+
+# Load models
+require_relative 'models/pokemon'
+
+get '/' do
+  @pokemon = Pokemon.all
+  erb :"pokemon/index"
+end
+
+get '/pokemon/new' do
+  erb :"pokemon/new"
+end
+
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemon/pokemon"
+end
+
+post '/pokemon' do
+  @pokemon = Pokemon.create(name: params[:name], cp: params[:cp], poke_type: params[:poke_type], img_url: params[:img_url])
+  redirect "/pokemon/#{@pokemon.id}"
+end
+
+get '/pokemon/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb(:"pokemon/edit")
+end
+
+put '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:id])
+  redirect "/pokemon/#{@pokemon.id}"
+end
+
+delete '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect "/"
+end
