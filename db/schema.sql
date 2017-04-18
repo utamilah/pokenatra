4c9d8ec7c3b8e0ad0e10865418ad2def251a540a
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-18 12:03:35 -0400

    add schema info for trainer

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,18 @@
 DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
+
+CREATE TABLE trainers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  level INTEGER,
+  img_url VARCHAR
+);
 
 CREATE TABLE pokemons(
   id SERIAL PRIMARY KEY,
   name VARCHAR,
-  cp INT,
+  cp INTEGER,
   poke_type VARCHAR,
-  img_url VARCHAR
+  img_url VARCHAR,
+  trainer_id INTEGER
 );
